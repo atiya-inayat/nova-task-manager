6b9e1bd40536b38e4668e41027b9c9e83b2d430f
atiya-inayat <atiyainayat869@gmail.com>
2026-01-02 21:39:35 +0500

    next-auth complete setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,7 @@
 // api endpoint for next-auth
+
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,80 @@
 // this file configures NextAuth with credientials providers, callbacks and session management
+
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { connectDB } from "./db";
+import User from "@/models/User";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      // authorize() - This is where login actually happens.
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing credentials");
+        }
+
+        // coneect to db
+        await connectDB();
+        // Find user in DB
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        // Check if blocked
+        if (user.isBlocked) {
+          throw new Error("User is blocked");
+        }
+        // Compare password
+        const isPasswordValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordValid) {
+          throw new Error("Invalid password");
+        }
+
+        // Return user data → session created
+        return {
+          id: user._id.toString(),
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    // jwt callback → runs when token is created
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = (user as any).role;
+      }
+      return token;
+    },
+    // session callback → controls what frontend receives
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
+  },
+};
+
+// “How do you add custom fields to NextAuth session?”
+// “By extending NextAuth types using module augmentation.”
