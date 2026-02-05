6d61a8b7448788f658a471c28154f60247022fca
atiya-inayat <atiyainayat869@gmail.com>
2026-02-05 22:00:52 +0500

    build: final successful database and type fixes

--- lib/db.ts
+++ lib/db.ts
@@ -1,52 +1,92 @@
-// this is db connection file , it will contain code for connecting app with db through mongoose , reuse existing connection and avoid multiple connection
+// // this is db connection file , it will contain code for connecting app with db through mongoose , reuse existing connection and avoid multiple connection
 
+// // 1. import mongoose
+// // 2. read MONGO_URI
+// // check if mongo-uri exist - if connection exist , return it - else create connection and cache it
+// // 3. create cached object
+// // 4. create connectDB() function
+// // 5. export it
 
-// 1. import mongoose 
-// 2. read MONGO_URI
-// check if mongo-uri exist - if connection exist , return it - else create connection and cache it 
-// 3. create cached object
-// 4. create connectDB() function 
-// 5. export it 
+// import mongoose from "mongoose";
+// const MONGO_URI = process.env.MONGODB_URI!;
+
+// if (!MONGO_URI) {
+//     throw new Error("Please define mongo_url in environment variable ")
+// }
+
+// let cached = global.mongoose
+
+// if (!cached) {
+//     cached = global.mongoose = {conn: null, promise: null}
+// }
+
+// export async function connectDB() {
+//     if (cached.conn) {
+//                 console.log("db connected");
+
+//         return cached.conn
+//     }
+
+//     if (!cached.promise) {
+//         const options = {
+//             bufferCommands: false,
+//             maxPoolSize: 10
+//         }
+
+//         cached.promise = mongoose
+//             .connect(MONGO_URI, options)
+//             .then(() => mongoose.connection)
+//     }
+
+//     try {
+//         cached.conn = await cached.promise
+//         console.log("db connected");
+
+//     } catch (error) {
+//         cached.promise = null
+//         throw error
+//     }
+
+//     return cached.conn
+// }
 
 import mongoose from "mongoose";
+
 const MONGO_URI = process.env.MONGODB_URI!;
 
 if (!MONGO_URI) {
-    throw new Error("Please define mongo_url in environment variable ")
+  throw new Error("Please define MONGODB_URI in environment variable");
 }
 
-let cached = global.mongoose
-
-if (!cached) {
-    cached = global.mongoose = {conn: null, promise: null}
+// Ensure the global object is initialized immediately
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
 }
+
+const cached = global.mongoose;
 
 export async function connectDB() {
-    if (cached.conn) {
-                console.log("db connected");
+  // Use 'cached!' to tell TypeScript that cached is NOT undefined
+  if (cached!.conn) {
+    return cached!.conn;
+  }
 
-        return cached.conn
-    }
+  if (!cached!.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
 
-    if (!cached.promise) {
-        const options = {
-            bufferCommands: false,
-            maxPoolSize: 10
-        }
+    cached!.promise = mongoose.connect(MONGO_URI, opts).then((m) => {
+      return m;
+    });
+  }
 
-        cached.promise = mongoose
-            .connect(MONGO_URI, options)
-            .then(() => mongoose.connection)
-    }
+  try {
+    cached!.conn = await cached!.promise;
+  } catch (e) {
+    cached!.promise = null;
+    throw e;
+  }
 
-    try {
-        cached.conn = await cached.promise
-        console.log("db connected");
-        
-    } catch (error) {
-        cached.promise = null
-        throw error
-    }
-
-    return cached.conn
+  return cached!.conn;
 }
--- types.d.ts
+++ types.d.ts
@@ -1,13 +1,26 @@
-// we declare types inside this file 
+// we declare types inside this file
 // it is a TypeScript declaration file Its only job is to teach TypeScript about things that exist globally.
 
-import { Connection } from "mongoose";
+// import { Connection } from "mongoose";
 
-declare global { // it means I want to add something to the global scope of Node.js.
-    var mongoose: { // global variable - not mongoose library - This is our own object to store connection info
-        conn: Connection | null; // conn - stores the connection object if connection succeeds, otherwise it remains null.
-        promise: Promise<Connection> | null // promise stores a pending database connection - While MongoDB is connecting, we store the promise
-    }
+// declare global { // it means I want to add something to the global scope of Node.js.
+//     var mongoose: { // global variable - not mongoose library - This is our own object to store connection info
+//         conn: Connection | null; // conn - stores the connection object if connection succeeds, otherwise it remains null.
+//         promise: Promise<Connection> | null // promise stores a pending database connection - While MongoDB is connecting, we store the promise
+//     }
+// }
+
+// export {}
+
+import { Mongoose } from "mongoose";
+
+declare global {
+  var mongoose:
+    | {
+        conn: any;
+        promise: Promise<any> | null;
+      }
+    | undefined;
 }
 
-export {}
+export {};
